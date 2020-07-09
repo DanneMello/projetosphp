0b63e50af6b44d6f7b80986f123a3bf340942b62
MrDanne <55359309+Mr-Danne@users.noreply.github.com>
2020-07-09 00:55:00 -0300

    Array multidimensional

--- json/json.php
+++ json/json.php
@@ -1,5 +1,13 @@
 <?php
+$json = file_get_contents("https://api.hgbrasil.com/weather");
+$json = json_decode($json);
 
+// Array com array multidimensional
+$array = array("a" => 'lol', "b"=> 'lolol', "arrayDimensional"=>array("arr" =>'bingo', "arrayZ" =>array("arrr" =>'Zeta')));
 
-
+print_r ($array);
+//echo "Dados pegos: ".$json->results->temp->date['09/07'];
+//var_dump($json);
+echo "<br/>";
+//var_dump($array["a"]);
 ?>

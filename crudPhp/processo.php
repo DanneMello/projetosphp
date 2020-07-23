07421ca3bde0cd69d790765c0250edb3114daf9f
MrDanne <55359309+Mr-Danne@users.noreply.github.com>
2020-07-23 10:35:03 -0300

    Contagem das listas salvas  no meu banco

--- crudPhp/processo.php
+++ crudPhp/processo.php
@@ -8,7 +8,7 @@ include_once("conexao.php");
 
     $resultado = 0;
 
-
+// @Verificando se o questionário foi preenchido corretamente
     if(!isset($perg1) || !isset($perg2) || !isset($perg3) || !isset($perg4) ) {
     echo "Complete o questionário. :/ ";
     } else {
@@ -17,26 +17,30 @@ include_once("conexao.php");
 
 // @Inserido os dados informados pelo usuário no meu BD
     $sql = "INSERT INTO clientes SET faixaEtaria = '$perg1', tipoConvenio = '$perg2', faixaSalarial = '$perg3', motEmprestimo = '$perg4' ";
-        $sql = $pdo->query($sql);
-        echo "usuário inserido com sucesso<hr>";
+    $sql = $pdo->query($sql);
+    echo "usuário inserido com sucesso<hr>";
 
-//@Selecionando todos os dados da tabela clientes do meu BD
+// @Selecionando todos os dados da tabela clientes do meu BD
+    $total = 0;
+    $n = 0;
     $sql = "SELECT * FROM clientes ";
     $sql = $pdo->query($sql);
+    $total = $sql->rowCount(); // @Contando quantas listas tem na minha tabela
 
-    if($sql->rowCount() > 0) {
+    echo "O número de usuários cadastrados no meu Banco é: <b>". $total."</b><hr>";
 
-        foreach($sql->fetchAll() as $cliente) {
-            
-            echo " Faixa etária: ".$cliente["faixaEtaria"]."</br>"." Tipo de convênio: ".$cliente["tipoConvenio"]."</br>"." Faixa salarial: ".$cliente["faixaSalarial"]."</br>"." Motivo do empréstimo: ".$cliente["motEmprestimo"]."</br>";
+// @Buscando todos os dados da minha tabela e criando uma array "$row" 
+    foreach ($sql->fetchAll() as $row) {
+        echo "Cliente = ".$n."</br>";
+        echo " - Id : ". $row['id']."</br>";
+        echo " - Faixa etária : ". $row['faixaEtaria']."</br>";
+        echo " - Faixa salarial : ". $row['faixaSalarial']."</br>";
+        echo " - Motivo do empréstimo : ". $row['motEmprestimo']."</br>";
 
-            echo "<hr><hr>";
-
-
-        }
-    } else {
-        echo "Não há usuários cadastrados no banco<hr>";
+        echo "</br><hr>";
+        $n++;
     }
+
 
 }
 

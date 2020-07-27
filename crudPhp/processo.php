9e2025dc05102a71f5cbe6ca7dce9483ce893204
MrDanne <55359309+Mr-Danne@users.noreply.github.com>
2020-07-26 22:39:58 -0300

    Criado formulário HTML através do próprio PHP.

--- crudPhp/processo.php
+++ crudPhp/processo.php
@@ -158,8 +158,8 @@ include_once("conexao.php");
 
 
 
-
-        echo "TOTAL DE PESQUISAS REALIZADAS: <b>[   " . $total . "   ]</b><hr>";
+/*
+        echo " <h2 style='color: #A020F0 ; text-align: center;' > TOTAL DE PESQUISAS REALIZADAS: </h2>" . " <h1 style='color: #4B0082 ;text-align: center;'> $total </h1> <hr>";
 
             echo "<h2>FAIXA ETÁRIA</h2>";
             echo "<b>ATÉ 30 ANOS:</b>  ".number_format($Qt_FxE_A, 2)."%</br>";       
@@ -191,6 +191,178 @@ include_once("conexao.php");
             echo "<b>TIPO CONVÊNIO:</b>  ".$tipoConvenio."</br>";
             echo "<b>FAIXA SALARIAL:</b>  ".$faixaSalarial."</br>";
             echo "<b>MOTIVO DO EMPRÉSTIMO:</b>  ".$motEmprestimo."</br>";
+
+*/            
+            echo "<body bgcolor='#B0E0E6''> 
+    
+            <table width='1000' border='1px' style=' margin:auto; 
+            background: #40E0D0;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            margin-right: -50%;
+            transform: translate(-50%, -50%)' >
+        
+                <tr bgcolor='#A020F0'>
+
+                <tr>
+                <td height='40' COLSPAN='6' style=' text-align: center; font-size: 30px;'><b>FAIXA ETÁRIA</b></td>
+                </tr>
+        
+                <td width='101' height='40' style=' text-align: center;'>TOTAL:</td>
+                <td width='113' bgcolor=' #4Bff82' style=' text-align: center; font-size: 35px;'> <b> " . $total . "</b></td>
+                <td width='140' style=' text-align: center; font-size: 20px;'>ATÉ 30: " . number_format($Qt_FxE_A, 2) . "%</td>
+                <td width='85'  style=' text-align: center; font-size: 20px;'>DE 30 A 50: " .number_format($Qt_FxE_B, 2). "%</td>
+                <td width='140' style=' text-align: center; font-size: 20px;'>DE 50 A 65: ".number_format($Qt_FxE_C, 2)."%</td>
+                <td width='85'  style=' text-align: center; font-size: 20px;'>ACIMA DE 65: ".number_format($Qt_FxE_D, 2)."%</td>
+        
+                </tr>
+        
+                <tr>
+                <td height='40' COLSPAN='6' style=' text-align: center; font-size: 30px;'><b>TIPO DO CONVÊNIO</b></td>
+                </tr>
+        
+                <tr>
+                <td height='40'>  </td>
+                <td>INSS</td>
+                <td> ?% ATÉ 30</td>
+                <td> ?% DE 30 A 50</td>
+                <td> ?% DE 50 A 65</td>
+                <td> ?% ACIMA DE 65</td>
+                
+                </tr>
+        
+                <tr>
+        
+                <td height='40'>  </td>
+                <td>SIAPE</td>
+                <td> ?% ATÉ 30</td>
+                <td> ?% DE 30 A 50</td>
+                <td> ?% DE 50 A 65</td>
+                <td> ?% ACIMA DE 65</td>
+                
+                </tr>
+        
+                <tr>
+        
+                <td height='40'>  </td>
+                <td>FORÇAS ARMADAS</td>
+                <td> ?% ATÉ 30</td>
+                <td> ?% DE 30 A 50</td>
+                <td> ?% DE 50 A 65</td>
+                <td> ?% ACIMA DE 65</td>
+                
+                </tr>
+        
+                <tr>
+        
+                <td height='40'>  </td>
+                <td>OUTROS</td>
+                <td> ?% ATÉ 30</td>
+                <td> ?% DE 30 A 50</td>
+                <td> ?% DE 50 A 65</td>
+                <td> ?% ACIMA DE 65</td>
+                
+                </tr>
+        
+                <tr>
+                <td height='40' COLSPAN='6' style=' text-align: center; font-size: 30px;'><b>FAIXA SALARIAL</b></td>
+                </tr>
+        
+                <tr>
+        
+                <td height='40'>  </td>
+                <td>ATÉ 2 SALÁRIOS MÍNIMOS</td>
+                <td> ?% ATÉ 30</td>
+                <td> ?% DE 30 A 50</td>
+                <td> ?% DE 50 A 65 </td>
+                <td> ?% ACIMA DE 65</td>
+        
+                </tr>
+        
+                <tr>
+                
+                <td height='40'>  </td>
+                <td>DE 2 A 4 SALÁRIOS MÍNIMOS</td>
+                <td> ?% ATÉ 30</td>
+                <td> ?% DE 30 A 50</td>
+                <td> ?% DE 50 A 65 </td>
+                <td> ?% ACIMA DE 65</td>
+        
+                </tr>
+        
+                <tr>
+                
+                <td height='40'>  </td>
+                <td>DE 4 A 6 SALÁRIOS MÍNIMOS</td>
+                <td> ?% ATÉ 30</td>
+                <td> ?% DE 30 A 50</td>
+                <td> ?% DE 50 A 65 </td>
+                <td> ?% ACIMA DE 65</td>
+        
+                </tr>
+        
+                <tr>
+                
+                <td height='40'>  </td>
+                <td>ACIMA DE SALÁRIOS MÍNIMOS</td>
+                <td> ?% ATÉ 30</td>
+                <td> ?% DE 30 A 50</td>
+                <td> ?% DE 50 A 65 </td>
+                <td> ?$ ACIMA DE 65</td>
+        
+                </tr>
+        
+                <tr>
+                <td height='40' COLSPAN='6' style=' text-align: center; font-size: 30px;'> <b>MOTIVO DO EMPRÉSTIMO</b> </td>
+                </tr>
+        
+                <tr>
+                <td height='40'>  </td>
+                <td>PAGAR CONTA</td>
+                <td> ?% ATÉ 30 </td>
+                <td> ?% DE 30 A 50</td>
+                <td> ?% DE 50 A 65</td>
+                <td> ?% ACIMA DE 65</td>
+                </tr>
+        
+                <tr>
+                <td height='40'>  </td>
+                <td>REFORMA NA CASA</td>
+                <td> ?% ATÉ 30 </td>
+                <td> ?% DE 30 A 50</td>
+                <td> ?% DE 50 A 65</td>
+                <td> ?% ACIMA DE 65</td>
+                </tr>
+        
+                <tr>
+                <td height='40'>  </td>
+                <td>AQUISIÇÃO DE VEÍCULOS</td>
+                <td> ?% ATÉ 30 </td>
+                <td> ?% DE 30 A 50</td>
+                <td> ?% DE 50 A 65</td>
+                <td> ?% ACIMA DE 65</td>
+                </tr>
+        
+                <tr>
+                <td height='40'>  </td>
+                <td>OUTROS</td>
+                <td> ?% ATÉ 30 </td>
+                <td> ?% DE 30 A 50</td>
+                <td> ?% DE 50 A 65</td>
+                <td> ?% ACIMA DE 65</td>
+                </tr>
+        
+        
+        
+        
+            </table>
+        
+        
+        
+        
+        </body>";
+        
 
 
     }
---
+++ crudPhp/tabela.php
@@ -0,0 +1,179 @@
+<?php
+$total = 1000;
+
+    echo "<body bgcolor='#B0E0E6''> 
+    
+    <table width='1000' border='1px' style=' margin:auto; 
+    background: #40E0D0;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    margin-right: -50%;
+    transform: translate(-50%, -50%)' >
+
+        <tr bgcolor='#A020F0'>
+
+        <td width='101' height='40' style=' text-align: center;'>TOTAL:</td>
+        <td width='113' bgcolor=' #4B0082' style=' text-align: center;'> " . $total . "</td>
+        <td width='140' style=' text-align: center;'>ATÉ 30: " . number_format($Qt_FxE_A, 2) . "%</td>
+        <td width='85'  style=' text-align: center;'>DE 30 A 50: " .number_format($Qt_FxE_B, 2). "%</td>
+        <td width='140' style=' text-align: center;'>DE 50 A 65: ".number_format($Qt_FxE_C, 2)."%</td>
+        <td width='85'  style=' text-align: center;'>ACIMA DE 65: ".number_format($Qt_FxE_D, 2)."%</td>
+
+        </tr>
+
+        <tr>
+        <td height='40' COLSPAN='6' style=' text-align: center; font-size: 30px;'><b>TIPO DO CONVÊNIO</b></td>
+        </tr>
+
+        <tr>
+        <td height='40'>  </td>
+        <td>INSS</td>
+        <td> ?% ATÉ 30</td>
+        <td> ?% DE 30 A 50</td>
+        <td> ?% DE 50 A 65</td>
+        <td> ?% ACIMA DE 65</td>
+        
+        </tr>
+
+        <tr>
+
+        <td height='40'>  </td>
+        <td>SIAPE</td>
+        <td> ?% ATÉ 30</td>
+        <td> ?% DE 30 A 50</td>
+        <td> ?% DE 50 A 65</td>
+        <td> ?% ACIMA DE 65</td>
+        
+        </tr>
+
+        <tr>
+
+        <td height='40'>  </td>
+        <td>FORÇAS ARMADAS</td>
+        <td> ?% ATÉ 30</td>
+        <td> ?% DE 30 A 50</td>
+        <td> ?% DE 50 A 65</td>
+        <td> ?% ACIMA DE 65</td>
+        
+        </tr>
+
+        <tr>
+
+        <td height='40'>  </td>
+        <td>OUTROS</td>
+        <td> ?% ATÉ 30</td>
+        <td> ?% DE 30 A 50</td>
+        <td> ?% DE 50 A 65</td>
+        <td> ?% ACIMA DE 65</td>
+        
+        </tr>
+
+        <tr>
+        <td height='40' COLSPAN='6' style=' text-align: center; font-size: 30px;'><b>FAIXA SALARIAL</b></td>
+        </tr>
+
+        <tr>
+
+        <td height='40'>  </td>
+        <td>ATÉ 2 SALÁRIOS MÍNIMOS</td>
+        <td> ?% ATÉ 30</td>
+        <td> ?% DE 30 A 50</td>
+        <td> ?% DE 50 A 65 </td>
+        <td> ?% ACIMA DE 65</td>
+
+        </tr>
+
+        <tr>
+        
+        <td height='40'>  </td>
+        <td>DE 2 A 4 SALÁRIOS MÍNIMOS</td>
+        <td> ?% ATÉ 30</td>
+        <td> ?% DE 30 A 50</td>
+        <td> ?% DE 50 A 65 </td>
+        <td> ?% ACIMA DE 65</td>
+
+        </tr>
+
+        <tr>
+        
+        <td height='40'>  </td>
+        <td>DE 4 A 6 SALÁRIOS MÍNIMOS</td>
+        <td> ?% ATÉ 30</td>
+        <td> ?% DE 30 A 50</td>
+        <td> ?% DE 50 A 65 </td>
+        <td> ?% ACIMA DE 65</td>
+
+        </tr>
+
+        <tr>
+        
+        <td height='40'>  </td>
+        <td>ACIMA DE SALÁRIOS MÍNIMOS</td>
+        <td> ?% ATÉ 30</td>
+        <td> ?% DE 30 A 50</td>
+        <td> ?% DE 50 A 65 </td>
+        <td> ?$ ACIMA DE 65</td>
+
+        </tr>
+
+        <tr>
+        <td height='40' COLSPAN='6' style=' text-align: center; font-size: 30px;'> <b>MOTIVO DO EMPRÉSTIMO</b> </td>
+        </tr>
+
+        <tr>
+        <td height='40'>  </td>
+        <td>PAGAR CONTA</td>
+        <td> ?% ATÉ 30 </td>
+        <td> ?% DE 30 A 50</td>
+        <td> ?% DE 50 A 65</td>
+        <td> ?% ACIMA DE 65</td>
+        </tr>
+
+        <tr>
+        <td height='40'>  </td>
+        <td>REFORMA NA CASA</td>
+        <td> ?% ATÉ 30 </td>
+        <td> ?% DE 30 A 50</td>
+        <td> ?% DE 50 A 65</td>
+        <td> ?% ACIMA DE 65</td>
+        </tr>
+
+        <tr>
+        <td height='40'>  </td>
+        <td>AQUISIÇÃO DE VEÍCULOS</td>
+        <td> ?% ATÉ 30 </td>
+        <td> ?% DE 30 A 50</td>
+        <td> ?% DE 50 A 65</td>
+        <td> ?% ACIMA DE 65</td>
+        </tr>
+
+        <tr>
+        <td height='40'>  </td>
+        <td>OUTROS</td>
+        <td> ?% ATÉ 30 </td>
+        <td> ?% DE 30 A 50</td>
+        <td> ?% DE 50 A 65</td>
+        <td> ?% ACIMA DE 65</td>
+        </tr>
+
+
+
+
+    </table>
+
+
+
+
+</body>";
+
+
+
+
+
+
+
+
+
+
+?>

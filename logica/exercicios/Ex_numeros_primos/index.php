49f1d7a66457b8f4036c93af793a5e3dc5e15bb3
MrDanne <55359309+Mr-Danne@users.noreply.github.com>
2020-07-31 11:51:40 -0300

    Exercícios números primos funcionando

--- logica/exercicios/Ex_numeros_primos/index.php
+++ logica/exercicios/Ex_numeros_primos/index.php
@@ -4,21 +4,23 @@ $valor = $_POST["valor"];
 $enviar = $_POST['enviar'];
 $nome = strtoupper($nome);  // @Transformando os caracteres recebidos para maiúsculos.
 $count = 1;
-$coutDiv = 0;
+$countDiv = 0;
 
+do {
 
-    do {
-        if( $valor % $count == 0) {
-            $coutDiv =$coutDiv + 1;
-        }
-        $count = $count + 1;
+    if($valor % $count == 0) { // @Verificando se o resto da divisão do valor informado pelo usuário dividido pelo contador for igual a 0, Se sim, será incrementado mais um na váriavel $countDiv
+        $countDiv ++;
+    }
 
-    } while($count < $valor);
+    $count ++;
+} while ($count <= $valor);
 
-         echo "Existe $coutDiv  ";
+if($countDiv == 2) { // @Se for divisiveis apenas 2 vezes, no caso por 1 e ele mesmo, é considerado um primo
+    echo "</br><h1 style='text-align: center'> $valor </h1><h2 style='text-align: center'> É um número primo</h2>";
 
-
-// echo "<hr>$nome!! Segue o fatorial de $valor = $count <b> - </b>";
-
+    } else {         // @Senão não
+        echo "</br><h1 style='text-align: center'> $valor </h1><h2 style='text-align: center'> Não é considerado um número primo por ser divisível mais de 2 vezes</h2>";
+        
+    }
 
 ?>

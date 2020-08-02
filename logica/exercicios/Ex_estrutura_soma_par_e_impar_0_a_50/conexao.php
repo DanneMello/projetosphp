07205f09863e4b23c3ae25a23752e8859e32026d
MrDanne <55359309+Mr-Danne@users.noreply.github.com>
2020-08-02 14:52:12 -0300

    Exercicio contar valores pares e impares que estão entre 0 e 50 (Nesse exemplo).

---
+++ logica/exercicios/Ex_estrutura_soma_par_e_impar_0_a_50/conexao.php
@@ -0,0 +1,15 @@
+<?php
+// @Abaixo estou atribuindo ás váriaveis $dsn, $dbuser e $dbpass a localização do meu DB e meu login em senha.
+$banco = "mysql:dbname=usuarios;host=127.168.0.1";
+$dbuser = "root";
+$dbpass = "";
+
+try {
+    $pdo = new PDO($banco, $dbuser, $dbpass); // @Aqui estou usando as váriaveis por questão de uma melhor visualização do meu código.
+echo "Conectado ao banco: </br>";
+}       catch(PDOException $e){
+            echo "Conexão com o banco FALHOU:".$e->getMessage();
+
+        }
+        
+?>
---
+++ logica/exercicios/Ex_estrutura_soma_par_e_impar_0_a_50/index.php
@@ -0,0 +1,56 @@
+<?php
+require 'conexao.php';
+// @Pegando os valores informados pelo usuário via método POST
+    $nome = $_POST["nome"];
+    $valor = $_POST["valor"];
+    $email = $_POST["e-mail"];
+    $enviar = $_POST['enviar'];
+
+// @Verificando se o questionário foi preenchido corretamente
+if(!isset($nome) || !isset($valor) || !isset($email) ) {
+
+    echo "Complete o questionário. :/ ";
+
+} else {
+// @Inserindo os dados informados pelo usuário no meu BD
+        $sql = "INSERT INTO users SET nome = '$nome', email = '$email', valor = '$valor' ";
+        $sql = $pdo->query($sql);
+
+// @Selecionando todos os dados da tabela pessoa do meu BD
+        $total = 0;
+        $sql = "SELECT * FROM users  ";
+        $sql = $pdo->query($sql);
+        $total = $sql->rowCount(); // @Contando quantas listas tem na minha tabela
+
+// @Inicializando as váriaveis que irei armazenar os dados 
+        $total_0_A_50 = 0 ;
+        $somaImpar = 0 ;
+        $somaPar = 0 ;
+
+// @Usando um foreach para pegar os conteúdos salvos no DB e atribuir as variáveis
+        foreach ( $sql->fetchAll() as $row ) { 
+
+                $nome = $row['nome'];
+                $valor = $row['valor'];            
+                $email = $row['email'];
+
+            if( ($valor > 0) && ($valor < 50) ) {
+                $total_0_A_50 ++;
+
+// @Somando todos os valores impares e pares entre 0 e 50.
+                    if($valor % 2 == 0) {
+                        $somaPar += $valor;
+                            } else {
+                                $somaImpar += $valor;
+                            }
+
+            }
+
+        }
+        echo "</br>Total de usuários salvos no DB: $total";
+            echo "Existem $total_0_A_50 entre 0 e 50 valores registrados no DB.";
+                echo "</br>a soma dos números IMPARES digitados é: $somaImpar";
+                    echo "</br>a soma dos números PARES digitados é: $somaPar";
+
+}
+?>

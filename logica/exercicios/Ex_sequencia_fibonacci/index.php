c8892b1198765ad5161c79b17d0bb33208d173d6
MrDanne <55359309+Mr-Danne@users.noreply.github.com>
2020-08-05 07:21:50 -0300

    Exercício sequência de Fibonacci

--- logica/exercicios/Ex_sequencia_fibonacci/index.php
+++ logica/exercicios/Ex_sequencia_fibonacci/index.php
@@ -1,4 +1,18 @@
 <?php
+// @Sequência de fibonacci
+$nome = $_POST["nome"];
+$Qt_Numero = $_POST['valor'];
+$count =1;
+$anterior =0;
+$atual =1;
+$proximo =1;
 
+echo "<h2>$nome, você digitou $Qt_Numero e abaixo segue a sequência dos valores.</h2></br>";
+for ($count; $count < $Qt_Numero; $count++) {
+    echo "  $proximo  | ";
+        $proximo = $atual + $anterior;
+            $anterior = $atual;
+                $atual = $proximo;
+}
 
 ?>

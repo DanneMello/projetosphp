a3436722f3416687ad8fc01974c4456761102feb
MrDanne <55359309+Mr-Danne@users.noreply.github.com>
2020-08-15 15:02:13 -0300

    Delete Ok

--- projetoz/conexao.php
+++ projetoz/conexao.php
@@ -1,12 +1,12 @@
 <?php
-class Banco {   // @Criando classe banco (CRUD) que será responsável por efetuar a conexão com o DB
-    private $pdo;   // @Criando uma variável privada para guardar a conexão com o DB
+class Banco {       // @Criando classe banco (CRUD) que será responsável por efetuar a conexão com o DB
+    private $pdo;       // @Criando uma variável privada para guardar a conexão com o DB
     private $numRows;
     private $arrays;
     
-    public function __construct($host, $dbname, $dbuser, $dbpass) {   // @Criando um método publico onde irei fazer a conexão com o DB
+    public function __construct($host, $dbname, $dbuser, $dbpass) {     // @Criando um método publico onde irei fazer a conexão com o DB
         try {
-            $this->pdo = new PDO("mysql:dbname=".$dbname.";host=".$host, $dbuser, $dbpass);    // @Criando um construtor 'CRUD' de mySql dentro do try catch                
+            $this->pdo = new PDO("mysql:dbname=".$dbname.";host=".$host, $dbuser, $dbpass);     // @Criando um construtor 'CRUD' de mySql dentro do try catch                
             echo "Conexão com o DB Ok!</br></br>";
         } catch(PDOException $e) {
             echo "Falhou a conexão".$e->getMessage();
@@ -14,51 +14,63 @@ class Banco {   // @Criando classe banco (CRUD) que será responsável por efetu
     }
 
     public function query($sql) {
-        $query = $this->pdo->query($sql); // @Criando minha query 
+        $query = $this->pdo->query($sql);       // @Criando minha query 
         $this->numRows = $query->rowCount();
-        $this->array = $query->fetchAll(); // @Como não sei quantos itens vai retornar da minha consulta, estou usando o fetchAll que retornará todos os itens para minha array
+        $this->array = $query->fetchAll();      // @Como não sei quantos itens vai retornar da minha consulta, estou usando o fetchAll que retornará todos os itens para minha array
     }
 
     public function result() {
-        return $this->array;    // @Aqui o método result está retornando a array
+        return $this->array;        // @Aqui o método result está retornando a array
     }
 
     public function numRows() {
         return $this->numRows;
     }
 
-    public function insert($table, $data) { // @Aqui estou criando o método insert
-        if( !empty($table) && ( is_array($data) && count($data) > 0 ) ) { // @Verificando se a tabela foi preenchida, se $data é um array e se tem algo nela.
-            $sql = "INSERT INTO " .$table. " SET "; // @Montando minha query       
-            $dados = array(); // @Criei uma variavel auxiliar 'array' 
-            foreach($data as $chave => $valor) { // @Foreach para pegar cada campo da minha tabela que vão ser alterados
-                $dados[] = $chave." = '".addslashes($valor)."'"; // @Aqui estou usando o addslashes só para não dar problema com as aspas simples, feito isso terei um array com todo os campos que irão ser inserido 
+    public function insert($table, $data) {     // @Aqui estou criando o método insert
+        if( !empty($table) && ( is_array($data) && count($data) > 0 ) ) {       // @Verificando se a tabela foi preenchida, se $data é um array e se tem algo nela.
+            $sql = "INSERT INTO " .$table. " SET ";     // @Montando minha query       
+            $dados = array();       // @Criei uma variavel auxiliar 'array' 
+            foreach($data as $chave => $valor) {        // @Foreach para pegar cada campo da minha tabela que vão ser alterados
+                $dados[] = $chave." = '".addslashes($valor)."'";        // @Aqui estou usando o addslashes só para não dar problema com as aspas simples, feito isso terei um array com todo os campos que irão ser inserido 
             }
-            $sql = $sql.implode(", ", $dados); // @Implode concatenando com minha extrutura inicial
-            $this->pdo->query($sql); // @Executando meu insert
+            $sql = $sql.implode(", ", $dados);      // @Implode concatenando com minha extrutura inicial
+            $this->pdo->query($sql);        // @Executando meu insert
         }
     }
 
-    public function update($table, $data, $where= array(), $where_cond = "AND") { // @Caso o usuário não especifique onde será alterado os dados na tabela, o 3° parâmetro será definido como um array eo 4° parâmetro como um 'AND'
-
-        if(!empty($table) && ( is_array($data) && count($data) > 0 ) && is_array($where) ) {  // @Verificando se a tabela foi preenchida, se $data é uma array, se tem algo nela e se $where é um array
+    public function update($table, $data, $where= array(), $where_cond = "AND") {       // @Caso o usuário não especifique onde será alterado os dados na tabela, o 3° parâmetro será definido como um array eo 4° parâmetro como um 'AND'
+        if(!empty($table) && ( is_array($data) && count($data) > 0 ) && is_array($where) ) {        // @Verificando se a tabela foi preenchida, se $data é uma array, se tem algo nela e se $where é um array
             $sql = "UPDATE " .$table. " SET ";
-            $dados = array(); // @Criei uma variavel auxiliar 'array' 
-            foreach($data as $chave => $valor) { // @Foreach para pegar cada campo da minha tabela que serão alterados
-                $dados[] = $chave." = '".addslashes($valor)."'"; // @Aqui estou usando o addslashes só para não dar problema com as aspas simples, feito isso terei um array com todo os campos que irão ser inserido 
+            $dados = array();       // @Criei uma variavel auxiliar 'array' 
+            foreach($data as $chave => $valor) {        // @Foreach para pegar cada campo da minha tabela que serão alterados
+                $dados[] = $chave." = '".addslashes($valor)."'";        // @Aqui estou usando o addslashes só para não dar problema com as aspas simples, feito isso terei um array com todo os campos que irão ser inserido 
             }
-            $sql = $sql.implode(", ", $dados); // @Implode concatenando com minha extrutura inicial
+            $sql = $sql.implode(", ", $dados);       // @Implode concatenando com minha extrutura inicial
 
-            if(count($where) > 0) { // @Verificando se há campos especificado que será feito o update
-        
-                $dados = array(); // @Posso usar o mesmo $dados, pois ele já foi zerado
+            if(count($where) > 0) {         // @Verificando se há campos especificado que será feito o update       
+                $dados = array();        // @Posso usar o mesmo $dados, pois ele já foi zerado
                 foreach($where as $chave => $valor) {
                     $dados[] = $chave." = '".addslashes($valor)."'"; 
                 }
                 $sql = $sql." WHERE ".implode(" ".$where_cond." ", $dados); 
             }
+            $this->pdo->query($sql);        // @Executando minha query
+        }
+    }
 
-            $this->pdo->query($sql); // @Executando minha query
+    public function delete($table, $where, $where_cond = "AND") {
+        if( !empty($table) && ( is_array($where) && count($where) > 0 ) ) {
+            $sql = "DELETE FROM ".$table;
+
+            if(count($where) > 0) {         // @Verificando se há campos especificado que será feito o update       
+                $dados = array();       // @Posso usar o mesmo $dados, pois ele já foi zerado
+                foreach($where as $chave => $valor) {
+                    $dados[] = $chave." = '".addslashes($valor)."'"; 
+                }
+                $sql = $sql." WHERE ".implode(" ".$where_cond." ", $dados); 
+            }
+            $this->pdo->query($sql);
         }
     }
 }
--- projetoz/index.php
+++ projetoz/index.php
@@ -1,26 +1,25 @@
 <?php
-require_once "conexao.php"; // @Chamando o arquivo responsável pela conexão com o DB
+require_once "conexao.php";     // @Chamando o arquivo responsável pela conexão com o DB
 
-$banco = new Banco("localhost", "produtos", "root", "");
-
+$banco = new Banco("localhost", "produtos", "root", "");        // @Conectando ao DB
 
 $banco->query("SELECT * FROM tb_categoria_produto ");
 
-if ($banco->numRows() > 0){ // @Caso não retorne nenhum resultado do DB, o programa não executará esse trecho de código
+if ($banco->numRows() > 0){     // @Caso não retorne nenhum resultado do DB, o programa não executará esse trecho de código
 
-    foreach($banco->result() as $tb_categoria_produto) { // @Aqui estou usando um foreach para percorrer toda minha array 
+    foreach($banco->result() as $tb_categoria_produto) {        // @Aqui estou usando um foreach para percorrer toda minha array 
 
         echo "ID: ". $tb_categoria_produto['id_categoria_planejamento']. "</br>";
         echo "Categoria: " . $tb_categoria_produto['nome_categoria'] . "</br>";
         echo "Código do produto: ". $tb_categoria_produto['PK_tb_categoria_produto']. "<hr/>";
     }
+
 } else {
     echo "Sem resultado no DB";
 }
 
-
 /*
-$banco->insert("tb_categoria_produto", array( // @Inserindo dados 
+$banco->insert("tb_categoria_produto", array(       // @Inserindo dados 
     "id_categoria_planejamento" =>'',
     "nome_categoria" => 'esporte',
     "PK_tb_categoria_produto" => '3'
@@ -28,9 +27,15 @@ $banco->insert("tb_categoria_produto", array( // @Inserindo dados
 
 echo "Inserido com sucesso! ";
 */
+ 
+/*
 // @Atualizando dados do DB e especificando o 3° parâmetro (No caso estou passando o id_categoria_planejamento" =>'5') 
 $banco->update("tb_categoria_produto", 
     array("nome_categoria"=>'animal'), 
     array("id_categoria_planejamento"=>'3'));
+
+*/
+
+$banco->delete("tb_categoria_produto", array("id_categoria_planejamento"=>'6') );
 
 ?>
